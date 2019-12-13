195a9b7908c86a2ecacab92499301b710e8f04d1
hirofuji <hirofuji@preferred.jp>
2019-12-13 10:33:22 +0900

    fix subscriber warning

--- src/controller.hpp
+++ src/controller.hpp
@@ -1,24 +1,27 @@
 #pragma once
+#include "geometry_msgs/msg/pose.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include <iostream>
 #include <memory>
-#include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/pose.hpp"
 
-namespace ros2_mock
-{
-  class ControllerNode: public rclcpp::Node
-  {
-    public:
-      ControllerNode() : rclcpp::Node("controller") {
-        sub = this->create_subscription<geometry_msgs::msg::Pose>("raw_pose", listenCallback, rmw_qos_profile_default);
-      }
+using std::placeholders::_1;
 
-      static void listenCallback(const geometry_msgs::msg::Pose::SharedPtr pose){
-        std::cout << pose->position.x << std::endl;
-      }
+namespace ros2_mock {
+class ControllerNode : public rclcpp::Node {
+public:
+  ControllerNode() : rclcpp::Node("controller") {
+    //   sub = this->create_subscription<geometry_msgs::msg::Pose>(
+    //       "raw_pose", listenCallback, rmw_qos_profile_default);
+    sub = this->create_subscription<geometry_msgs::msg::Pose>(
+        "raw_pose", 10, std::bind(&ControllerNode::listenCallback, this, _1));
+  }
 
-    private:
-      std::shared_ptr<rclcpp::Subscription<geometry_msgs::msg::Pose>> sub;
-  };
+  void listenCallback(const geometry_msgs::msg::Pose::SharedPtr pose) {
+    std::cout << pose->position.x << std::endl;
+  }
 
-}
+private:
+  std::shared_ptr<rclcpp::Subscription<geometry_msgs::msg::Pose>> sub;
+}; // namespace ros2_mock
+
+} // namespace ros2_mock
